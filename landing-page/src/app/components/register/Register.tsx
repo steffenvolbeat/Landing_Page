3a26641497be82b6b298ecb6793f3f8fbdf805cb
MarcelSingh <Marcel.singh@rocketmail.com>
2025-09-29 09:52:40 +0200

    swiper darkMode

--- landing-page/src/app/components/register/Register.tsx
+++ landing-page/src/app/components/register/Register.tsx
@@ -11,11 +11,11 @@ const Register: React.FC = () => {
 
   return (
     <section
-      className="w-full flex  min-h-fit justify-around items-center  bg-[#F5f7fa]  sm:flex-col sm:pb-10 sm:pt-10 md:flex-row md:pl-10 md:pr-10 lg:flex-row"
+      className="mt-20 w-full flex  min-h-fit justify-around items-center  bg-[var(--main-background)] sm:flex-col sm:pb-10 sm:pt-10 md:flex-row md:pl-10 md:pr-10 lg:flex-row"
     >
       <div className=" flex flex-col gap-6 justify-center">
         <h1 className="text-5xl font-bold" style={{color: "var(--h-color)"}}>Lessons and insights</h1>
-        <p className="text-5xl font-bold" style={{ color: "var(--akzentcolor)" }}>from 8 years</p>
+        <span className="text-5xl font-bold" style={{ color: "var(--akzentcolor)" }}>from 8 years</span>
         <p className="text-xs">
           Where to grow your business as a photographer: site or social media?
         </p>
